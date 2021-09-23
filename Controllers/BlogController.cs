f1677538ed20d3fe967c62e66a70ab2fb815c041
sona94 <sonuvpatel94@gmail.com>
2021-09-23 16:21:15 -0500

    commit message

---
+++ Controllers/BlogController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using DailyBlogger.Models;
+
+
+namespace DailyBlogger.Controllers
+{
+    public class BlogController : Controller
+    {
+        private BlogContext _context;
+
+        public BlogController(BlogContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Details(int id)
+        {
+            BlogPost blogPost = _context.blogPost.Find(id);
+            return View(blogPost);
+        }
+
+        public IActionResult List()
+        {
+            IEnumerable<BlogPost> posts =   _context.blogPost.ToList<BlogPost>();
+            return View(posts);
+        }
+
+        public IActionResult New()
+        {
+            BlogPost blogPost = new BlogPost();
+
+            return View(blogPost);
+
+        }
+
+        public IActionResult Edit(int id)
+        {
+            BlogPost blogPost = _context.blogPost.Find(id);
+            return View(blogPost);
+        }
+
+
+        public IActionResult Delete([Bind("id")] int id)
+        {
+            BlogPost blogPost = _context.blogPost.Find(id);
+            _context.Remove(blogPost);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(List));
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Update([Bind("blogTitle,content,blogDate,id")] BlogPost blog)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Update(blog);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(List));
+            }
+            return View(blog);
+        }
+
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create([Bind("blogTitle,content,blogDate")] BlogPost blog)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(blog);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(List));
+            }
+            return View(blog);
+        }
+
+    }
+}
---
+++ Models/BlogPost.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace DailyBlogger.Models
+{
+    public class BlogPost
+    {
+        [Key]
+        public int id {get; set;}
+        [Display(Name = "Post Title")]
+        public string blogTitle { get; set; }
+        [Display(Name = "Write Blog Post Below")]
+        public string content { get; set; }
+        [Display(Name = "Date")]
+        public DateTime blogDate { get; set; }
+    }
+}
